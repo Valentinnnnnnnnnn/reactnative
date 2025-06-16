991a8c1c495e71425653692a654bc691638a9a8c
Valentin <valgaillard46@gmail.com>
2025-06-16 15:11:20 +0200

    C9 Status bar

--- TutorialExpo/StickerSmash/app/(tabs)/index.tsx
+++ TutorialExpo/StickerSmash/app/(tabs)/index.tsx
@@ -1,21 +1,21 @@
-import domtoimage from 'dom-to-image';
+import { ImageSourcePropType, View, StyleSheet, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import * as MediaLibrary from 'expo-media-library';
-import { useRef, useState } from 'react';
-import { ImageSourcePropType, Platform, StyleSheet, View } from 'react-native';
+import { useState, useRef } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
- 
+import domtoimage from 'dom-to-image';
+
 import Button from '@/components/Button';
-import CircleButton from '@/components/CircleButton';
-import EmojiList from '@/components/EmojiList';
-import EmojiPicker from '@/components/EmojiPicker';
-import EmojiSticker from '@/components/EmojiSticker';
-import IconButton from '@/components/IconButton';
 import ImageViewer from '@/components/ImageViewer';
- 
+import IconButton from '@/components/IconButton';
+import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
+import EmojiList from '@/components/EmojiList';
+import EmojiSticker from '@/components/EmojiSticker';
+
 const PlaceholderImage = require('@/assets/images/background-image.png');
- 
+
 export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
@@ -23,18 +23,18 @@ export default function Index() {
   const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
   const [status, requestPermission] = MediaLibrary.usePermissions();
   const imageRef = useRef<View>(null);
- 
+
   if (status === null) {
     requestPermission();
   }
- 
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
       quality: 1,
     });
- 
+
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
       setShowAppOptions(true);
@@ -42,19 +42,19 @@ export default function Index() {
       alert('You did not select any image.');
     }
   };
- 
+
   const onReset = () => {
     setShowAppOptions(false);
   };
- 
+
   const onAddSticker = () => {
     setIsModalVisible(true);
   };
- 
+
   const onModalClose = () => {
     setIsModalVisible(false);
   };
- 
+
   const onSaveImageAsync = async () => {
     if (Platform.OS !== 'web') {
       try {
@@ -62,7 +62,7 @@ export default function Index() {
           height: 440,
           quality: 1,
         });
- 
+
         await MediaLibrary.saveToLibraryAsync(localUri);
         if (localUri) {
           alert('Saved!');
@@ -77,7 +77,7 @@ export default function Index() {
           width: 320,
           height: 440,
         });
- 
+
         let link = document.createElement('a');
         link.download = 'sticker-smash.jpeg';
         link.href = dataUrl;
@@ -87,7 +87,7 @@ export default function Index() {
       }
     }
   };
- 
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
@@ -116,7 +116,7 @@ export default function Index() {
     </GestureHandlerRootView>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -134,8 +134,8 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 80,
   },
-    optionsRow: {
-      alignItems: 'center',
-      flexDirection: 'row',
-    }
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
 });
