30b28e6381bfffe86c9c0eb6e4061c984de767d4
Valentin <valgaillard46@gmail.com>
2025-06-18 14:14:54 +0200

    debug persistance

--- TicketManagerApp/config/firebase.config.ts
+++ TicketManagerApp/config/firebase.config.ts
@@ -1,14 +1,20 @@
 //import { getAnalytics } from "firebase/analytics";
-import firebaseConfig from '@/.env'
+import firebaseConfig from '@/.env';
+import { initializeApp } from 'firebase/app';
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { initializeApp } from 'firebase/app'
-import { initializeAuth } from 'firebase/auth'
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+
 //const auth = getAuth(app);
-const auth = initializeAuth(app)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+})
+// Set persistence for Firebase Auth
+// setPersistence(auth, indexedDBLocalPersistence);
 
 //const analytics = getAnalytics(app);
 
-export { app, auth }
+export { app, auth };
